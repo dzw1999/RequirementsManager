a12c9cac2b4d235f418c0d5968b5f19bbdfa2e6d
dzw1999 <958451206@qq.com>
2021-07-07 00:07:59 +0800

    07070007-为了spread不fake开始改

--- rm-server/filemanager/filemanager/api/file/spreaddetect/getSpreadData.py
+++ rm-server/filemanager/filemanager/api/file/spreaddetect/getSpreadData.py
@@ -2,4 +2,11 @@ def getSpreadData(strupath:str,relapath:str):
     res=""
     with open(relapath,"r",encoding='UTF-8') as f:
         res=f.read()
-    return res
+    fakeres=""
+    fakeresfile=r"C:\Users\wang9\Desktop\2\fakeresfile.txt"
+    with open(fakeresfile,"r",encoding='UTF-8') as f:
+        # lines=f.readlines()
+        # for line in lines:
+        #     fakeres+=line+"\n"
+        fakeres=f.read()
+    return fakeres
